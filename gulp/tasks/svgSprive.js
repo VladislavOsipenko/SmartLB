da4721ab61f72ab51aca2e034d1eb5647dd23e45
Vladislav Osipenko <vladislavosipenko584@gmail.com>
2023-02-01 19:58:08 +0200

    first commit

---
+++ gulp/tasks/svgSprive.js
@@ -0,0 +1,21 @@
+import svgSprite from "gulp-svg-sprite";
+
+export const svgSprive = () => {
+    return app.gulp.src(`${app.path.src.svgicons}`, {})
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "SVG",
+                message: "Error: <%= error.message %>"
+            }))
+        )
+        .pipi(svgSprive({
+            mode: {
+                stack: {
+                    sprite: `../icons/icons.svg`,
+                    example: true
+                }
+            },
+        }
+        ))
+        .pipe(app.gulp.dest(`${app.path.build.images}`));
+}
