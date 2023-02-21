86cfc5ac785da4d82cc81a0b47918ec4e7d0ace1
Vladislav Osipenko <vladislavosipenko584@gmail.com>
2023-02-21 19:32:58 +0200

    done project

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
-const build = gulp.series(reset, mainTasks);
+const build = gulp.series(reset, mainTasks, server);
 const deployZip = gulp.series(reset, mainTasks, zip);
 const deployFTP = gulp.series(reset, mainTasks, ftp);
 
--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,32 @@ flsFunctions.isWebp();
 
 import Swiper, { Navigation, Pagination } from "swiper";
 
+
 const swiper = new Swiper('.swiper', {
     
 });
+
+"use strict";
+
+window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.header__menu'),
+        menuItem = document.querySelectorAll('.header__li'),
+        hamburger = document.querySelector('.header__hamburger');
+    
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('header__hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        });
+    
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('header__hamburger_active');
+                menu.classList.toggle('header__menu_active');
+            })
+        });
+
+    new WOW().init();
+});
+
+
+
